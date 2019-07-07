a7942647c3459e84b1a6f3d53f65d8b06b9cce14
bgamut <bgamut@gmail.com>
2019-07-07 13:14:21 +0900

    first commit

---
+++ build/precache-manifest.1b09a18bd7ac32fbd51027e2ab38b3ee.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bf9817a75c34c98478e97a05b6aaf44",
+    "url": "/index.html"
+  },
+  {
+    "revision": "333bcd4b832e6ce22a58",
+    "url": "/static/js/2.ac8b418b.chunk.js"
+  },
+  {
+    "revision": "e59e69fb8df3fc34edd2",
+    "url": "/static/js/main.8cbf7b26.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
